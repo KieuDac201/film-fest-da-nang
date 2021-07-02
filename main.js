9a938e60c871dbb75ce86ab140dc59bb472f10ce
KieuDac <kieudac18082001@gmail.com>
2021-07-02 09:19:02 +0700

    done homepage

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const navE = document.querySelector('.nav')
+const navIcon = document.querySelector('.nav__icon')
+const navMenu = document.querySelector('.nav__menu')
 
 let isScroll = false
 
@@ -9,3 +11,12 @@ window.onscroll = function(e) {
         navE.classList.remove('active')
     }
 }
+navIcon.addEventListener('click', () => {
+    if(navMenu.classList.value.includes('active')){
+        navIcon.innerHTML = '<i class="fas fa-bars"></i>'
+        navMenu.classList.remove('active')
+    }else{
+        navIcon.innerHTML = '<i class="fas fa-times"></i>'
+        navMenu.classList.add('active')
+    }
+})

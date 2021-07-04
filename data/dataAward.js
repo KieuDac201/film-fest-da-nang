0fbba11238ee003456ed3dee6b0c4787600a74da
KieuDac <kieudac18082001@gmail.com>
2021-07-04 19:55:42 +0700

    complete giai thuong page

---
+++ data/dataAward.js
@@ -0,0 +1,55 @@
+const dataAward = {
+  special: [
+    {
+      img: "../images/giaithuong/award-special.png",
+      h4: "1 giải đặc biệt - Giải Sông Hàn",
+      h5: "100.000.000.000 VNĐ (một trăm triệu đồng)",
+      p: [
+        "Vé máy bay khứ hồi và Voucher lưu trú khách sạn (tiêu chuẩn 4 - 5 sao) dành cho ekip sản xuất (không quá 5 người)",
+        " Biểu tượng Cuộc thi Phim ngắn Đà Nẵng Lần thứ I – 2021 “KHÁM PHÁ ĐÀ NẴNG TUYỆT VỜI - WOW DA NANG” ",
+      ],
+    },
+  ],
+  gold: [
+    {
+      img: "../images/giaithuong/award-gold1.png",
+      h4: "1 giải Cầu Rồng",
+      h5: "30.000.000.000 VNĐ (ba mươi triệu đồng)",
+      p: [
+        "Vé máy bay khứ hồi và Voucher lưu trú khách sạn (tiêu chuẩn 4 - 5 sao) dành cho ekip sản xuất (không quá 5 người)",
+        " Biểu tượng Cuộc thi Phim ngắn Đà Nẵng Lần thứ I – 2021 “KHÁM PHÁ ĐÀ NẴNG TUYỆT VỜI - WOW DA NANG” ",
+      ],
+    },
+    {
+      img: "../images/giaithuong/award-gold2.png",
+      h4: "1 giải Sông Hàn",
+      h5: "100.000.000.000 VNĐ (một trăm triệu đồng)",
+      p: [
+        "Vé máy bay khứ hồi và Voucher lưu trú khách sạn (tiêu chuẩn 4 - 5 sao) dành cho ekip sản xuất (không quá 5 người)",
+        " Biểu tượng Cuộc thi Phim ngắn Đà Nẵng Lần thứ I – 2021 “KHÁM PHÁ ĐÀ NẴNG TUYỆT VỜI - WOW DA NANG” ",
+      ],
+    },
+    {
+      img: "../images/giaithuong/award-gold3.png",
+      h4: "1 giải Cầu Thuận Phước",
+      h5: "100.000.000.000 VNĐ (một trăm triệu đồng)",
+      p: [
+        "Vé máy bay khứ hồi và Voucher lưu trú khách sạn (tiêu chuẩn 4 - 5 sao) dành cho ekip sản xuất (không quá 5 người)",
+        " Biểu tượng Cuộc thi Phim ngắn Đà Nẵng Lần thứ I – 2021 “KHÁM PHÁ ĐÀ NẴNG TUYỆT VỜI - WOW DA NANG” ",
+      ],
+    },
+  ],
+  silver: [
+    {
+      img: "../images/giaithuong/award-silver.png",
+      h4: "5 giải Digital",
+      h5: "10.000.000.000 VNĐ (mười triệu đồng)",
+      p: [
+        "Có lượt tương tác nhiều nhất trên các nền tảng số: Website, Mobile App, Youtube, Fanpage, Tik Tok,…",
+        " Biểu tượng Cuộc thi Phim ngắn Đà Nẵng Lần thứ I – 2021 “KHÁM PHÁ ĐÀ NẴNG TUYỆT VỜI - WOW DA NANG” ",
+      ],
+    },
+  ],
+};
+
+export default dataAward;

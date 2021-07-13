d1740b860150d32767330e4fca924f7d8fdf553b
KieuDac <kieudac18082001@gmail.com>
2021-07-13 21:05:09 +0700

    nothing

--- main.js
+++ main.js
@@ -1,29 +1,32 @@
-const navE = document.querySelector('.nav')
-const navIcon = document.querySelector('.nav__icon')
-const navMenu = document.querySelector('.nav__menu')
-const btnDropdown = document.querySelector('.nav__menu-dropdown .nav__menu-link')
-const dropdownE = document.querySelector('.dropdown-list')
+const navE = document.querySelector(".nav");
+const navIcon = document.querySelector(".nav__icon");
+const navMenu = document.querySelector(".nav__menu");
+const btnDropdown = document.querySelector(
+  ".nav__menu-dropdown .nav__menu-link"
+);
+const dropdownE = document.querySelector(".dropdown-list");
+const btnRegister = document.querySelector("btn-register");
 
-let isScroll = false
+let isScroll = false;
 
-window.onscroll = function(e) {
-    if(document.body.scrollTop > 0 || document.documentElement.scrollTop > 0){
-        navE.classList.add('active')
-    }else{
-        navE.classList.remove('active')
-    }
-}
-navIcon.addEventListener('click', () => {
-    if(navMenu.classList.value.includes('active')){
-        navIcon.innerHTML = '<i class="fas fa-bars"></i>'
-        navMenu.classList.remove('active')
-    }else{
-        navIcon.innerHTML = '<i class="fas fa-times"></i>'
-        navMenu.classList.add('active')
-    }
-})
+window.onscroll = function (e) {
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+    navE.classList.add("active");
+  } else {
+    navE.classList.remove("active");
+  }
+};
+navIcon.addEventListener("click", () => {
+  if (navMenu.classList.value.includes("active")) {
+    navIcon.innerHTML = '<i class="fas fa-bars"></i>';
+    navMenu.classList.remove("active");
+  } else {
+    navIcon.innerHTML = '<i class="fas fa-times"></i>';
+    navMenu.classList.add("active");
+  }
+});
 
-btnDropdown.addEventListener('click', (e) => {
-    e.preventDefault()
-    dropdownE.classList.toggle('active')
-})
+btnDropdown.addEventListener("click", (e) => {
+  e.preventDefault();
+  dropdownE.classList.toggle("active");
+});

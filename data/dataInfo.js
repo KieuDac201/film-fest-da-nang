1172c4376272e464058a445a6d67d650b145d368
KieuDac <kieudac18082001@gmail.com>
2021-07-13 20:56:37 +0700

    done faq and tracuu page

---
+++ data/dataInfo.js
@@ -0,0 +1,46 @@
+const dataInfo = [
+  {
+    name: "Nguyễn Thu Thủy",
+    title: "Khám phá một ngày làm việc của người Đà Nẵng",
+    number: "001",
+    img: "/images/tracuu/info1.png",
+    type: "VIRAL CLIP",
+  },
+  {
+    name: "Nguyễn Thu Thủy",
+    title: "Khám phá một ngày làm việc của người Đà Nẵng",
+    number: "002",
+    img: "/images/tracuu/info2.png",
+    type: "PHIM NGẮN",
+  },
+  {
+    name: "Nguyễn Thu Thủy",
+    title: "Khám phá một ngày làm việc của người Đà Nẵng",
+    number: "003",
+    img: "/images/tracuu/info3.png",
+    type: "MV CA NHẠC   ",
+  },
+  {
+    name: "Nguyễn Thu Thủy",
+    title: "Khám phá một ngày làm việc của người Đà Nẵng",
+    number: "004",
+    img: "/images/tracuu/info4.png",
+    type: "PHÓNG SỰ",
+  },
+  {
+    name: "Nguyễn Thu Thủy",
+    title: "Khám phá một ngày làm việc của người Đà Nẵng",
+    number: "005",
+    img: "/images/tracuu/info5.png",
+    type: "VIRAL CLIP",
+  },
+  {
+    name: "Nguyễn Thu Thủy",
+    title: "Khám phá một ngày làm việc của người Đà Nẵng",
+    number: "006",
+    img: "/images/tracuu/info6.png",
+    type: "PHÓNG SỰ",
+  },
+];
+
+export default dataInfo;

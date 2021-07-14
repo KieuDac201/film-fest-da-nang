12113305535ee127f1f497583a360ebf0a4ff880
KieuDac <kieudac18082001@gmail.com>
2021-07-14 09:54:31 +0700

    form register done

--- main.js
+++ main.js
@@ -1,11 +1,62 @@
+const bodyE = document.querySelector("body");
+let bodyHtml = bodyE.innerHTML;
+let formHtml = `<div id="form">
+<div class="overlay"></div>
+<div class="register">
+  <div class="btn-close">
+    <i class="fas fa-times"></i>
+  </div>
+  <img src="./images/common/logo.png" alt="" class="logo" />
+  <h4>Tham gia cuộc thi phim ngắn Đà Nẵng lần thứ I-2021</h4>
+  <form class="form">
+    <div class="form-group">
+      <input type="text" name="email" placeholder="Địa chỉ email" />
+      <img src="./images/common/email-icon.png" alt="" />
+    </div>
+    <div class="form-group">
+      <input type="password" name="password" placeholder="Mật khẩu" />
+      <img src="./images/common/lock-icon.png" alt="" />
+    </div>
+    <div class="form-group">
+      <input
+        type="password"
+        name="password2"
+        placeholder="Nhập lại mật khẩu"
+      />
+      <img src="./images/common/lock-icon.png" alt="" />
+    </div>
+    <div class="form-checkbox">
+      <input type="checkbox" />
+      <p>Tôi đã đọc và đồng ý với <span>Điều khoản và Điều kiện</span></p>
+    </div>
+    <button class="btn">Đăng Kí</button>
+    <p>Hoặc đăng kí</p>
+    <button class="btn btn-third-party">
+      <img src="./images/common/fb-color-icon.png" alt="" /> Đăng kí bằng
+      Facebook
+    </button>
+    <button class="btn btn-third-party">
+      <img src="./images/common/gg-color-icon.png" alt="" /> Đăng kí bằng
+      Google
+    </button>
+    <p>Bạn đã có tài khoản? <span class="btn-login">Đăng nhập</span></p>
+  </form>
+</div>
+</div>`;
+bodyE.innerHTML = formHtml + bodyE.innerHTML;
 const navE = document.querySelector(".nav");
 const navIcon = document.querySelector(".nav__icon");
 const navMenu = document.querySelector(".nav__menu");
 const btnDropdown = document.querySelector(
   ".nav__menu-dropdown .nav__menu-link"
 );
+
 const dropdownE = document.querySelector(".dropdown-list");
-const btnRegister = document.querySelector("btn-register");
+const btnRegister = document.querySelector(".btn-register");
+
+let btnClose = document.querySelector(".btn-close");
+const formE = document.getElementById("form");
+let overlayE = document.querySelector(".overlay");
 
 let isScroll = false;
 
@@ -30,3 +81,20 @@ btnDropdown.addEventListener("click", (e) => {
   e.preventDefault();
   dropdownE.classList.toggle("active");
 });
+
+const openForm = () => {
+  btnRegister.addEventListener("click", () => {
+    formE.classList.add("active");
+  });
+};
+openForm();
+
+const closeForm = () => {
+  btnClose.addEventListener("click", () => {
+    formE.classList.remove("active");
+  });
+  overlayE.addEventListener("click", () => {
+    formE.classList.remove("active");
+  });
+};
+closeForm();
